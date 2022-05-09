579e6064e3706efb354e17d107356f27295c54e5
maishpie <96499709+maishpie@users.noreply.github.com>
2022-05-09 18:46:03 -0400

    cleared scripts.js and added array for pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-alert('Hello world');
-let favoriteFood= 'Thai';
-document.write(favoriteFood)
+let pokemonList= [{
+  name: 'Charizard',
+  height: 1.7,
+  type: ['fire','flying'],
+  abilities: ['blaze','solar power']
+},
+{
+  name: 'Jigglypuff',
+  height: 0.5,
+  type: ['fairy','normal'],
+  abilities: ['cute-charm','friend-guard']
+},
+{
+  name: 'Psyduck',
+  height: 0.8,
+  type: 'water',
+  abilities: ['damp','cloud-nine','Swift-swim']
+}
+];
