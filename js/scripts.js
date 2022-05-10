6e2a5ef7ee8e7233b2457a245edb7ebfef2c2ba5
maishpie <96499709+maishpie@users.noreply.github.com>
2022-05-10 19:13:21 -0400

    set up if statement to highlight special pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,12 @@ let pokemonList= [{
 }
 ];
 
-for (let i= 0; i< pokemonList.length; i++) {
-  document.write('That\'s a ' + pokemonList[i].name)
+for (let i= 0; i< pokemonList.length; i++) { //iterating through each object in the array
+  if (pokemonList[i].height > 1) { //highlighting special pokemon
+    document.write('<p>That\'s a ' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow! That\'s big!</p>')
+
+  } else {
+    document.write('<p>That\'s a ' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>')
+  }
+
 }
