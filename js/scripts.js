3b4089006aae699cd8a0d48aa8bfd1179cef490f
maishpie <96499709+maishpie@users.noreply.github.com>
2022-05-27 19:50:49 -0400

    implemented a forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -18,12 +18,9 @@ let pokemonList= [{
 }
 ];
 
-for (let i= 0; i< pokemonList.length; i++) { //iterating through each object in the array
-  if (pokemonList[i].height > 1) { //highlighting special pokemon
-    document.write('<p>That\'s a ' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow! That\'s big!</p>')
 
-  } else {
-    document.write('<p>That\'s a ' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>')
-  }
+pokemonList.forEach(function (pokemon) => {
+  document.write('<p>That\'s a ' + pokemon.name + ' (height: ' + pokemon.height + ')</p>')
 
-}
+
+});
