fd437661be6daf0c3d7f0de320dcab5911c06d14
maishpie <96499709+maishpie@users.noreply.github.com>
2022-06-15 16:32:28 -0400

    wrapped array in iife and created getAll and add functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,36 @@
-let pokemonList= [{
-  name: 'Charizard',
-  height: 1.7,
-  type: ['fire','flying'],
-  abilities: ['blaze','solar power']
-},
-{
-  name: 'Jigglypuff',
-  height: 0.5,
-  type: ['fairy','normal'],
-  abilities: ['cute-charm','friend-guard']
-},
-{
-  name: 'Psyduck',
-  height: 0.8,
-  type: 'water',
-  abilities: ['damp','cloud-nine','Swift-swim']
-}
-];
+let pokemonRepository = (function () { //wrapping in iife
+  let privPokemonList= [{
+    name: 'Charizard',
+    height: 1.7,
+    type: ['fire','flying'],
+    abilities: ['blaze','solar power']
+  },
+  {
+    name: 'Jigglypuff',
+    height: 0.5,
+    type: ['fairy','normal'],
+    abilities: ['cute-charm','friend-guard']
+  },
+  {
+    name: 'Psyduck',
+    height: 0.8,
+    type: 'water',
+    abilities: ['damp','cloud-nine','Swift-swim']
+  }
+  ];
 
+  return {
+    add: function(pokemon) {
+      privPokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return privPokemonList;
+    }
+  }
+
+})();
+
+let pokemonList= pokemonRepository.getAll();
 
 pokemonList.forEach(function (pokemon) {
   document.write('<p>That\'s a ' + pokemon.name + ' (height: ' + pokemon.height + ')</p>')
