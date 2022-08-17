4b4bbed87fb7d5f43e0133549c1b87ccbfc3b89f
maishpie <96499709+maishpie@users.noreply.github.com>
2022-08-16 20:04:43 -0400

    fixing linting issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 let pokemonRepository = (function () { //main IIFE
   let pokemonList= [];
-  let apiURL= "https://pokeapi.co/api/v2/pokemon/?limit=150";
+  let apiURL= 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add (pokemon) {
     if (
-      typeof pokemon === "object" && "name" in pokemon) {
+      typeof pokemon === 'object' && 'name' in pokemon) {
         pokemonList.push(pokemon);
       } else {
-        console.log("pokemon format is not correct");
+        console.log('pokemon format is not correct');
       }
   }
 
@@ -16,22 +16,24 @@ let pokemonRepository = (function () { //main IIFE
   }
 
   function addListItem (pokemon) {
-    let pokemonList = document.querySelector(".list-group");
-    let listItem = document.createElement("li");
-    listItem.classList.add("group-list-item");
-    let button = document.createElement("button");
+    let pokemonList = document.querySelector('.list-group');
+    let listItem = document.createElement('li');
+    listItem.classList.add('group-list-item');
+    let button = document.createElement('button');
     button.innerText = pokemon.name;
-    button.classList.add("btn");
-    button.classList.add("btn-block");
-    button.classList.add("btn-outline-dark");
-    button.classList.add("shadow");
-    button.setAttribute("data-toggle", "modal");
-    button.setAttribute("data-target", "#pokemonDetailsModal");
+    button.classList.add('btn');
+    button.classList.add('btn-block');
+    button.classList.add('btn-outline-dark');
+    button.classList.add('shadow');
+    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('data-target', '#pokemonDetailsModal');
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
-    button.addEventListener("click", function(event){
+    /* eslint-disable */
+    button.addEventListener('click', function(event){
       showDetails(pokemon);
     });
+    /* eslint-enable */
   }
 
   function loadList() {
@@ -74,20 +76,20 @@ let pokemonRepository = (function () { //main IIFE
   }
 
   //Modal setup and implementation here
+  /* eslint-disable */
   function showModal (pokemon) {
-    let modalBody = $(".modal-body");
-    let modalTitle = $(".modal-title");
-    let modalHeader = $(".modal-header");
+    let modalBody = $('.modal-body');
+    let modalTitle = $('.modal-title');
     modalTitle.empty();
     modalBody.empty();
 
-    let nameElement = $("<h1>" + pokemon.name + "</h1>");
+    let nameElement = $('<h1>' + pokemon.name + '</h1>');
     let imageElement = $('<img class="modal-img" style="width:50%">');
-    imageElement.attr("src", pokemon.imageUrl);
-    let idElement = $("<p>" + "ID# " + pokemon.id + "</p>");
-    let heightElement = $("<p>" + "height : " + pokemon.height/10 + " m" + "</p>");
-    let weightElement = $("<p>" + "weight : " + pokemon.weight/10 + " kg" + "</p>");
-    let typesElement = $("<p>" + "type(s) : " + pokemon.types + "</p>");
+    imageElement.attr('src', pokemon.imageUrl);
+    let idElement = $('<p>' + 'ID# ' + pokemon.id + '</p>');
+    let heightElement = $('<p>' + 'height : ' + pokemon.height/10 + ' m' + '</p>');
+    let weightElement = $('<p>' + 'weight : ' + pokemon.weight/10 + ' kg' + '</p>');
+    let typesElement = $('<p>' + 'type(s) : ' + pokemon.types + '</p>');
 
     modalTitle.append(nameElement);
     modalBody.append(imageElement);
@@ -95,7 +97,7 @@ let pokemonRepository = (function () { //main IIFE
     modalBody.append(heightElement);
     modalBody.append(weightElement);
     modalBody.append(typesElement);
-
+  /* eslint-enable */
   }
 
   return {
